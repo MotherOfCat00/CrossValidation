c7b4c126de9f42541cbee18ff999af9a0a6af6e0
MotherOfCat00 <asiakotwica245@gmail.com>
2023-04-15 19:27:12 +0200

    Poprawna wersja do sprawozdania

--- main.py
+++ main.py
@@ -18,167 +18,169 @@ from sklearn.model_selection import GroupKFold
 from sklearn.model_selection import LeaveOneGroupOut
 from sklearn.model_selection import LeavePGroupsOut
 from sklearn.model_selection import GroupShuffleSplit
+import sys
+np.set_printoptions(threshold=sys.maxsize)
 
-iris = datasets.load_breast_cancer()
-print(iris.data.shape, iris.target.shape)
+wine = datasets.load_wine()
 
 
-def custom_cv_2folds(X):
-    n = X.shape[0]
-    i = 1
-    while i <= 2:
-        idx = np.arange(n * (i-1)/2, n * i/2, dtype=int)
-        yield idx, idx
-        i += 1
+# def custom_cv_2folds(X):
+#     n = X.shape[0]
+#     i = 1
+#     while i <= 2:
+#         idx = np.arange(n * (i-1)/2, n * i/2, dtype=int)
+#         yield idx, idx
+#         i += 1
 
 
 X_train, X_test, y_train, y_test = train_test_split(
-    iris.data,
-    iris.target,
+    wine.data,
+    wine.target,
     test_size=0.4,
     random_state=0
 )
 
-print(X_train.shape, y_train.shape)
-print(X_test.shape, y_test.shape)
-
-clf = svm.SVC(kernel='linear', C=1).fit(X_train, y_train)
-print(clf.score(X_test, y_test))
-
-scores = cross_val_score(clf, iris.data, iris.target, cv=5, scoring='f1_macro')
-print(scores)
-print("Accuracy %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-
-n_samples = iris.data.shape[0]
-cv = ShuffleSplit(n_splits=5, test_size=0.3, random_state=0)
-print(cross_val_score(clf, iris.data, iris.target, cv=cv))
-
-custom_cv = custom_cv_2folds(iris.data)
-print(cross_val_score(clf, iris.data, iris.target, cv=custom_cv))
-
-scaler = preprocessing.StandardScaler().fit(X_train)
-X_train_transformed = scaler.transform(X_train)
-clf = svm.SVC(C=1).fit(X_train_transformed, y_train)
-X_test_transformed = scaler.transform(X_test)
-print(clf.score(X_test_transformed, y_test))
-
 clf = make_pipeline(preprocessing.StandardScaler(), svm.SVC(C=1))
-print(cross_val_score(clf, iris.data, iris.target, cv=cv))
 
-scoring = ['precision_macro', 'recall_macro']
-clf = svm.SVC(kernel='linear', C=1, random_state=0)
-scores = cross_validate(clf, iris.data, iris.target, scoring=scoring, cv=5, return_train_score=False)
-print(sorted(scores.keys()))
-print(scores['test_recall_macro'])
-
-scoring = {'prec_macro': 'precision_macro',
-           'rec_micro': make_scorer(recall_score, average='macro')}
-scores = cross_validate(clf, iris.data, iris.target, scoring=scoring, cv=5, return_train_score=True)
-print(sorted(scores.keys()))
-print(scores['train_rec_micro'])
-
-scores = cross_validate(clf, iris.data, iris.target, scoring='precision_macro', cv=5, return_estimator=True)
-print(sorted(scores.keys()))
+metrics = ['accuracy']
 
 # K-Fold
-X = ["a", "b", "c", "d"]
-kf = KFold(n_splits=2)
-for train, test in kf.split(X):
-    print("%s %s" % (train, test))
-
-X = np.array([[0., 0.], [1., 1.], [-1., -1.], [2., 2.]])
-y = np.array([0, 1, 0, 1])
-X_train, X_test, y_train, y_test = X[train], X[test], y[train], y[test]
+kf = KFold(n_splits=3)
+generator = kf.split(wine)
+# for train, test in kf.split(wine):
+#     print("%s %s" % (train, test))
+scores = cross_val_score(clf, wine.data, wine.target, cv=kf)
+print("walidacja k_fold")
+print("Accuracy %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores2 = cross_validate(clf, wine.data, wine.target, cv=kf, scoring=metrics)
+print("Accuracy %0.2f (+/- %0.2f)" % (scores2['test_accuracy'].mean(), scores2['test_accuracy'].std() * 2))
 
 # Repeated K-Fold
-X = np.array([[1, 2], [3, 4], [1, 2], [3, 4]])
 random_state = 12883823
-rkf = RepeatedKFold(n_splits=2, n_repeats=2, random_state=random_state)
-for train, test in rkf.split(X):
-    print("%s %s" % (train, test))
+rkf = RepeatedKFold(n_splits=3, n_repeats=2, random_state=random_state)
+# for train, test in rkf.split(wine):
+#     print("%s %s" % (train, test))
+scores = cross_val_score(clf, wine.data, wine.target, cv=rkf)
+print("walidacja rkf")
+print("Accuracy %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores2 = cross_validate(clf, wine.data, wine.target, cv=rkf, scoring=metrics)
+print("Accuracy %0.2f (+/- %0.2f)" % (scores2['test_accuracy'].mean(), scores2['test_accuracy'].std() * 2))
 
 # Leave One Out (LOO)
-X = [1, 2, 3, 4]
 loo = LeaveOneOut()
-for train, test in loo.split(X):
-    print("%s %s" % (train, test))
+# for train, test in loo.split(wine):
+#     print("%s %s" % (train, test))
+print("Walidacja loo:")
+scores = cross_val_score(clf, wine.data, wine.target, cv=loo)
+print("Accuracy %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores2 = cross_validate(clf, wine.data, wine.target, cv=loo, scoring=metrics)
+print("Accuracy %0.2f (+/- %0.2f)" % (scores2['test_accuracy'].mean(), scores2['test_accuracy'].std() * 2))
 
 # Leave P Out (LPO)
-X = np.ones(4)
 lpo = LeavePOut(p=2)
-for train, test in lpo.split(X):
-    print("%s %s" % (train, test))
+# for train, test in lpo.split(wine):
+#     print("%s %s" % (train, test))
+scores = cross_val_score(clf, wine.data, wine.target, cv=lpo)
+print("walidacja lpo")
+print("Accuracy %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores2 = cross_validate(clf, wine.data, wine.target, cv=lpo, scoring=metrics)
+print("Accuracy %0.2f (+/- %0.2f)" % (scores2['test_accuracy'].mean(), scores2['test_accuracy'].std() * 2))
 
 # Walidacja losowa permutacji krzyżowych Shuffle & Split
-X = np.arange(10)
-ss = ShuffleSplit(n_splits=5, test_size=0.25, random_state=0)
-for train_index, test_index in ss.split(X):
-    print("%s %s" % (train_index, test_index))
+ss = ShuffleSplit(n_splits=3, test_size=0.25, random_state=0)
+# for train_index, test_index in ss.split(wine):
+#     print("%s %s" % (train_index, test_index))
+scores = cross_val_score(clf, wine.data, wine.target, cv=ss)
+print("walidacja ss")
+print("Accuracy %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores2 = cross_validate(clf, wine.data, wine.target, cv=ss, scoring=metrics)
+print("Accuracy %0.2f (+/- %0.2f)" % (scores2['test_accuracy'].mean(), scores2['test_accuracy'].std() * 2))
 
 # Iteratory walidacji krzyżowej ze stratyfikacją opartą na etykietach klas
-# Stratified k-fold
 
-X, y = np.ones((50, 1)), np.hstack(([0] * 45, [1] * 5))
+# Stratified k-fold
 skf = StratifiedKFold(n_splits=3)
-for train, test in skf.split(X, y):
-    print('train -  {}   |   test -  {}'.format(
-        np.bincount(y[train]), np.bincount(y[test])))
+# for train, test in skf.split(wine.data, wine.target):
+#     print('train -  {}   |   test -  {}'.format(
+#         np.bincount(wine.target[train]), np.bincount(wine.target[test])))
+scores = cross_val_score(clf, wine.data, wine.target, cv=skf)
+print("walidacja skf:")
+print("Accuracy %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores2 = cross_validate(clf, wine.data, wine.target, cv=skf, scoring=metrics)
+print("Accuracy %0.2f (+/- %0.2f)" % (scores2['test_accuracy'].mean(), scores2['test_accuracy'].std() * 2))
 
 kf = KFold(n_splits=3)
-for train, test in kf.split(X, y):
-    print('train -  {}   |   test -  {}'.format(
-        np.bincount(y[train]), np.bincount(y[test])))
+# for train, test in kf.split(wine.data, wine.target):
+#     print('train -  {}   |   test -  {}'.format(
+#         np.bincount(wine.target[train]), np.bincount(wine.target[test])))
+scores = cross_val_score(clf, wine.data, wine.target, cv=kf)
+print("walidacja kf2:")
+print("Accuracy %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores2 = cross_validate(clf, wine.data, wine.target, cv=kf, scoring=metrics)
+print("Accuracy %0.2f (+/- %0.2f)" % (scores2['test_accuracy'].mean(), scores2['test_accuracy'].std() * 2))
 
-# Iteratory walidacji krzyżowej dla zgrupowanych danych
+# Iteratory w, walidacji krzyżowej dla zgrupowanych danych
 # Grupowanie k-fold
-X = [0.1, 0.2, 2.2, 2.4, 2.3, 4.55, 5.8, 8.8, 9, 10]
-y = ["a", "b", "b", "b", "c", "c", "c", "d", "d", "d"]
-groups = [1, 1, 1, 2, 2, 2, 3, 3, 3, 3]
-
+X = wine.data
+y = wine.target
+groups = np.rint(wine.data[:, 0])
 gkf = GroupKFold(n_splits=3)
-for train, test in gkf.split(X, y, groups=groups):
-    print("%s %s" % (train, test))
+# for train, test in gkf.split(X, y, groups=groups):
+#     print("%s %s" % (train, test))
+scores = cross_val_score(clf, wine.data, wine.target, cv=gkf, groups=groups)
+print("walidacja gkf:")
+print("Accuracy %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores2 = cross_validate(clf, wine.data, wine.target, cv=gkf, scoring=metrics, groups=groups)
+print("Accuracy %0.2f (+/- %0.2f)" % (scores2['test_accuracy'].mean(), scores2['test_accuracy'].std() * 2))
 
 # StratifiedGroupKFold
-X = list(range(18))
-y = [1] * 6 + [0] * 12
-groups = [1, 2, 3, 3, 4, 4, 1, 1, 2, 2, 3, 4, 5, 5, 5, 6, 6, 6]
 sgkf = StratifiedGroupKFold(n_splits=3)
-for train, test in sgkf.split(X, y, groups=groups):
-    print("%s %s" % (train, test))
+# for train, test in sgkf.split(X, y, groups=groups):
+#     print("%s %s" % (train, test))
+scores = cross_val_score(clf, wine.data, wine.target, cv=sgkf, groups=groups)
+print("walidacja sgkf")
+print("Accuracy %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores2 = cross_validate(clf, wine.data, wine.target, cv=sgkf, scoring=metrics, groups=groups)
+print("Accuracy %0.2f (+/- %0.2f)" % (scores2['test_accuracy'].mean(), scores2['test_accuracy'].std() * 2))
 
 # Leave One Group Out
-X = [1, 5, 10, 50, 60, 70, 80]
-y = [0, 1, 1, 2, 2, 2, 2]
-groups = [1, 1, 2, 2, 3, 3, 3]
 logo = LeaveOneGroupOut()
-for train, test in logo.split(X, y, groups=groups):
-    print("%s %s" % (train, test))
+# for train, test in logo.split(X, y, groups=groups):
+#     print("%s %s" % (train, test))
+scores = cross_val_score(clf, wine.data, wine.target, cv=logo, groups=groups)
+print("walidacja logo")
+print("Accuracy %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores2 = cross_validate(clf, wine.data, wine.target, cv=logo, scoring=metrics, groups=groups)
+print("Accuracy %0.2f (+/- %0.2f)" % (scores2['test_accuracy'].mean(), scores2['test_accuracy'].std() * 2))
 
 # Leave P Groups Out
-X = np.arange(6)
-y = [1, 1, 1, 2, 2, 2]
-groups = [1, 1, 2, 2, 3, 3]
 lpgo = LeavePGroupsOut(n_groups=2)
-for train, test in lpgo.split(X, y, groups=groups):
-    print("%s %s" % (train, test))
+# for train, test in lpgo.split(X, y, groups=groups):
+#     print("%s %s" % (train, test))
+scores = cross_val_score(clf, wine.data, wine.target, cv=lpgo, groups=groups)
+print("walidacja lpgo")
+print("Accuracy %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores2 = cross_validate(clf, wine.data, wine.target, cv=lpgo, scoring=metrics, groups=groups)
+print("Accuracy %0.2f (+/- %0.2f)" % (scores2['test_accuracy'].mean(), scores2['test_accuracy'].std() * 2))
 
 # Group Shuffle Split
-X = [0.1, 0.2, 2.2, 2.4, 2.3, 4.55, 5.8, 0.001]
-y = ["a", "b", "b", "b", "c", "c", "c", "a"]
-groups = [1, 1, 2, 2, 3, 3, 4, 4]
-gss = GroupShuffleSplit(n_splits=4, test_size=0.5, random_state=0)
-for train, test in gss.split(X, y, groups=groups):
-    print("%s %s" % (train, test))
+gss = GroupShuffleSplit(n_splits=3, test_size=0.5, random_state=0)
+# for train, test in gss.split(X, y, groups=groups):
+#     print("%s %s" % (train, test))
+scores = cross_val_score(clf, wine.data, wine.target, cv=gss, groups=groups)
+print("walidacja gss")
+print("Accuracy %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores2 = cross_validate(clf, wine.data, wine.target, cv=gss, scoring=metrics, groups=groups)
+print("Accuracy %0.2f (+/- %0.2f)" % (scores2['test_accuracy'].mean(), scores2['test_accuracy'].std() * 2))
 
 # Time Series Split
-X = np.array([[1, 2], [3, 4], [1, 2], [3, 4], [1, 2], [3, 4]])
-y = np.array([1, 2, 3, 4, 5, 6])
-tscv = TimeSeriesSplit(n_splits=3)
+tscv = TimeSeriesSplit(n_splits=4)
 print(tscv)
-
-for train, test in tscv.split(X):
-    print("%s %s" % (train, test))
+# for train, test in tscv.split(wine):
+#     print("%s %s" % (train, test))
+scores = cross_val_score(clf, wine.data, wine.target, cv=tscv)
+print("walidacja tscv", scores)
+print("Accuracy %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
 
 
